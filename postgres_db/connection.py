06e0a20538f3b52ff9c162b5c12bbcbf79375256
ilya.miheev <r1sephoenix@yandex.ru>
2023-10-26 09:51:30 +0300

    changes in funcions for db comection

--- postgres_db/connection.py
+++ postgres_db/connection.py
@@ -2,16 +2,21 @@ import psycopg2
 from sshtunnel import SSHTunnelForwarder
 import yaml
 
-with open('postgres_db/config.yml', 'r') as f:
+from pathlib import Path
+
+config_dir = Path(__file__).parent.parent.resolve() / 'config'
+
+with open(config_dir / 'config.yml', 'r') as f:
     config_yaml = yaml.safe_load(f)
 
-Host = config_yaml['Host']
+Host = config_yaml['Hostname']
 User = config_yaml['User']
 Path_to_ssh_key = config_yaml['Path_to_ssh_key']
-Db_name = config_yaml['vectordb']
-Db_user = config_yaml['testuser']
-Db_password = config_yaml['testpwd']
+Db_name = config_yaml['Db_name']
+Db_user = config_yaml['Db_user']
+Db_password = config_yaml['Db_password']
 Db_port = config_yaml['Db_port']
+
 
 def create_db_connection():
     tunnel = SSHTunnelForwarder(
@@ -19,20 +24,23 @@ def create_db_connection():
         ssh_username=User,
         ssh_private_key=Path_to_ssh_key,
         remote_bind_address=('localhost', Db_port),
-        local_bind_address=('localhost', 6543)
+        #local_bind_address=('localhost', 6543)
     )
     tunnel.start()
     try:
         conn = psycopg2.connect(
             database=Db_name,
             user=Db_user,
-            password=Db_password
+            password=Db_password,
+            port=Db_port
         )
         return tunnel, conn
     except (Exception, psycopg2.Error) as error:
-        print("Error while connecting", error)
+        print('Error while connecting', error)
     return None
 
-# conn.close()
-# tunnel.stop()
 
+if __name__ == '__main__':
+    tunnel, conn = create_db_connection()
+    conn.close()
+    tunnel.stop()
